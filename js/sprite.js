73c2faca225c64f76a0bd6796708eff31d6f098c
Андрей <upndrey@yandex.ru>
2020-03-13 19:55:59 +0700

    Готовый вариант простой игры из интернета

---
+++ js/sprite.js
@@ -0,0 +1,10 @@
+function Sprite(url, pos, size, speed, frames, dir, once) {
+    this.pos = pos;
+    this.size = size;
+    this.speed = typeof speed === 'number' ? speed : 0;
+    this.frames = frames;
+    this._index = 0;
+    this.url = url;
+    this.dir = dir || 'horizontal';
+    this.once = once;
+};
